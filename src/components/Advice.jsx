ca2fbd40af5a764cc847644ff2cace08662b3f2c
eneskalkann <eneskalkan36e@gmail.com>
2022-10-26 15:21:35 +0300

    desktop-design-done

--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -1,36 +1,48 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useState } from 'react'
-import IconDice from '../assets/icon-dice.svg'
-import DividerMobile from '../assets/pattern-divider-mobile.svg'
-import DividerDesktop from '../assets/pattern-divider-desktop.svg'
-import '../design/Advice.css'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import IconDice from "../assets/icon-dice.svg";
+import DividerMobile from "../assets/pattern-divider-mobile.svg";
+import DividerDesktop from "../assets/pattern-divider-desktop.svg";
+import "../design/Advice.css";
 
 function Advice() {
+  const [advice, setAdvice] = useState([]);
 
-    const [advice , setAdvice] = useState([]);
+  const fetchAdvice = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
 
-    const fetchAdvice = async () => {
-        const res = await fetch("https://api.adviceslip.com/advice")
-        const data = await res.json()
+    setAdvice(data.slip);
+  };
 
-        console.log(data.slip);
-        setAdvice(data.slip);
-    }
-
-    useEffect(() => {
-        fetchAdvice();
-    },[])
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
 
   return (
-    <div className='container'>
+    <div className="container">
+      <div className="container-advice">
         <h4>Advice # {advice.id}</h4>
         <p>"{advice.advice}"</p>
-        <button className='container-button' onClick={fetchAdvice}>
-            <img src={IconDice} alt="" />
+        <div className="container-advice-divider">
+          <img
+            src={DividerDesktop}
+            alt=""
+            className="container-advice-divider-desktop"
+          />
+          <img
+            src={DividerMobile}
+            alt=""
+            className="container-advice-divider-mobile"
+          />
+        </div>
+        <button className="container-advice-button" onClick={fetchAdvice}>
+          <img src={IconDice} alt="dice" />
         </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Advice
+export default Advice;
